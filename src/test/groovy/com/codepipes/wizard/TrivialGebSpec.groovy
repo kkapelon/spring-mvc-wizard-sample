380523b5ad599ecaaeaa7f7b0b4a502a0cbecd89
Kostis Kapelonis <kkapelon@gmail.com>
2014-11-17 23:55:49 +0200

    Split Geb test correctly using Spock blocks

--- src/test/groovy/com/codepipes/wizard/TrivialGebSpec.groovy
+++ src/test/groovy/com/codepipes/wizard/TrivialGebSpec.groovy
@@ -14,29 +14,31 @@ import geb.Browser
 class TrivialGebSpec extends GebSpec {
 
 	def "Trivial Geb test with Google"() {
-		when: "I search wikipedia in Google"
+		when: "I go to Google"
 		Browser.drive {
 			go "http://google.com/ncr"
-
+		}
+		
+		then: "Gooogle should load"
+		Browser.drive {
 			// make sure we actually got to the page
-			assert title == "Google"
-
-			// enter wikipedia into the search field
+			title == "Google"
+		}
+		
+		when: "I enter wikipedia in the search link"
+		Browser.drive {
 			$("input", name: "q").value("wikipedia")
 
 			// wait for the change to results page to happen
 			// (google updates the page dynamically without a new request)
 			waitFor { title.endsWith("Google Search") }
-
-			
-			
 		}
 
 
 		then: "First link should be wikipedia"
 		Browser.drive {
-		def firstLink = $("li.g", 0).find("a.l")
-		firstLink.text() == "Wikipedia"
+			def firstLink = $("li.g", 0).find("a.l")
+			firstLink.text() == "Wikipedia"
 		}
 	}
 
